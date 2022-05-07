975be2125eb1f46594a23b753cb0226b80a26b53
CChantal <vrienschantal@gmail.com>
2022-05-07 20:42:32 +0200

    website first try

---
+++ Javascipt.js
@@ -0,0 +1,37 @@
+
+
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+
+
+	//Animation
+
+	//Menu On Hover
+
+	$('body').on('mouseenter mouseleave','.nav-item',function(e){
+			if ($(window).width() > 750) {
+				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+				setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+				},1);
+			}
+	});
+
+	//Switch light/dark
+
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("dark")) {
+			$("body").removeClass("dark");
+			$("#switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("dark");
+			$("#switch").addClass("switched");
+		}
+	});
+
+  })(jQuery);
+
+/* Chantal */
